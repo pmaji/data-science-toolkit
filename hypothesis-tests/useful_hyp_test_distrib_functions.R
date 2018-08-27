d77790c96e64b92ab38605cf1038d39fb2634fa6
Paul Jeffries <paul.madison.jeffries@gmail.com>
2018-08-26 23:03:08 -0400

    small aesthetic change to labeling format in ks chart

--- hypothesis-tests/useful_hyp_test_distrib_functions.R
+++ hypothesis-tests/useful_hyp_test_distrib_functions.R
@@ -372,7 +372,7 @@ gen_ks_test_viz_and_results <-
     sub4_merged_plot <- cowplot::add_sub(
       plot = sub3_titled_merged_plot,
       label = paste0(
-        "Stat=",
+        "stat=",
         round(
           ks_test_stat,
           digits = decimal_place_for_test_stat

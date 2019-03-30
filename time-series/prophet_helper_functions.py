e571e7b152f079cc44d8a27846f788b75548c7bb
pmjgit <paul.madison.jeffries@gmail.com>
2019-03-30 19:12:13 -0400

    first draft of prophet vignette done

--- time-series/prophet_helper_functions.py
+++ time-series/prophet_helper_functions.py
@@ -3,7 +3,8 @@ import matplotlib.dates as mdates
 
 def dt_restricted_prophet_plt(
     m, fcst, visible_window_start_dt=None, visible_window_end_dt=None, ax=None,
-    uncertainty=True, plot_cap=True, xlabel='ds', ylabel='y', figsize=(10,6), x_axis_date_format = '%Y-%m'
+    uncertainty=True, plot_cap=True, xlabel='ds', ylabel='y', figsize=(10,6), 
+    x_axis_date_format = '%Y-%m', auto_fit_xaxis_labels = True
 ):
     """
     Plot the Prophet forecast, restricting the plot using optional start and end dates.
@@ -22,6 +23,8 @@ def dt_restricted_prophet_plt(
     xlabel: Optional label name on X-axis.
     ylabel: Optional label name on Y-axis.
     figsize: Optional tuple width, height in inches.
+    x_axis_date_format: Specifies the date format of the x-axis. 
+    auto_fit_xaxis_labels: Boolean indicating if x-axis lables should be auto-fit.
     
     Returns
     -------
@@ -62,18 +65,21 @@ def dt_restricted_prophet_plt(
         ax.fill_between(fcst_t, visible_forecast['yhat_lower'], visible_forecast['yhat_upper'],
                         color='#0072B2', alpha=0.2)
     ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
+    # label the axes
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    # interpret the x-axis values as dates
-    # Set the locator
-    locator = mdates.MonthLocator()  # every month
+    # set the locator (by default finds the months)
+    locator = mdates.MonthLocator()
     ax.xaxis.set_major_locator(locator)
-    # Specify the format - %b gives us Jan, Feb...
+    # specifies the date format to use in the x-axis formatter
     fmt = mdates.DateFormatter(x_axis_date_format)
-    # Specify formatter
+    # apply the date formatter to the x-axis
     ax.xaxis.set_major_formatter(fmt)
-    # make space for and rotate the x-axis tick labels
-    fig.autofmt_xdate() 
+    # make space for and rotates the x-axis tick labels if user wants
+    if auto_fit_xaxis_labels == True:
+        fig.autofmt_xdate() 
+    else:
+        pass
     fig.tight_layout()
     
     return fig
